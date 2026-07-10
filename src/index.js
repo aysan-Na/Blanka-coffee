89b4a16dd31b2c70c53277ac6f9fa4b0b11477aa
Aysan <aysannaderian8@gmail.com>
2026-07-10 12:31:47 +0330

    update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "./styles/variables.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Blanka-coffee">
     <App />
   </BrowserRouter>
 );
